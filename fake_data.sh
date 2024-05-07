6ddad915fc45e90ae96ce79ffc33cef59ee03614
ains-arch <ainsleearchibald@gmail.com>
2024-05-06 17:20:18 -0700

    fixed workflows

--- fake_data.sh
+++ fake_data.sh
@@ -12,4 +12,4 @@ num_rows_users=$2
 num_rows_tweets=$3
 
 # Run fake_data.py script with arguments in parallel
-time python3 fake_data.py $num_rows_urls $num_rows_users $num_rows_tweets &
+time python3 fake_data.py $num_rows_urls $num_rows_users $num_rows_tweets
